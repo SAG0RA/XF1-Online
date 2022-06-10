a488019eceaad4ee0c7f8aceb60783d63a7a8d7a
IIISAGORAIII <saulgora9@gmail.com>
2022-06-10 00:25:10 -0600

    Crear liga avance
    ayudame dios

--- src/FrontEnd/src/app/crear-liga/crear-liga.component.ts
+++ src/FrontEnd/src/app/crear-liga/crear-liga.component.ts
@@ -1,36 +1,85 @@
 
-import { Component,ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, OnDestroy } from '@angular/core';
 import { MatTable } from '@angular/material/table';
 
-import {MatDialog,MatDialogConfig} from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 
 export interface MiembroElement {
-  nombre:string
+  nombre: string
 }
 
-const MIEMBRO_DATA: MiembroElement[]=[];
+const MIEMBRO_DATA: MiembroElement[] = [];
 
 @Component({
   selector: 'app-crear-liga',
   templateUrl: './crear-liga.component.html',
   styleUrls: ['./crear-liga.component.css']
 })
-export class CrearLigaComponent implements OnInit {
+export class CrearLigaComponent implements OnInit, OnDestroy {
 
   dataSourceMiembro = MIEMBRO_DATA;
   @ViewChild('miembros') tablaMiembros: MatTable<MiembroElement>;
-  displayedColumnsEquipo:string[] = ['nombre','actions']; 
-  
+  displayedColumnsEquipo: string[] = ['nombre'];
+
+  nombreLiga:string
+  userNameInvitado: string
+  codigo:string
+
   constructor() { }
 
   ngOnInit(): void {
-    var sexo = {
-      nombre: 'El mejor'
+    var usuario = {
+      nombre: 'Usuario'
     }
-
-    MIEMBRO_DATA.push(sexo)
+    MIEMBRO_DATA.push(usuario)
     this.tablaMiembros.renderRows()
   }
 
+  ngOnDestroy() {
+    MIEMBRO_DATA.splice(0, MIEMBRO_DATA.length)
+  }
+
+  agregarUsuario() {
+    console.log(MIEMBRO_DATA)
+    if (this.existeMiembro(MIEMBRO_DATA, { nombre: this.userNameInvitado })) {
+      alert('El jugador ya ha sido agregado')
+    } else {
+
+      var usuarioNuevo = {
+        nombre: this.userNameInvitado
+      }
+      MIEMBRO_DATA.push(usuarioNuevo)
+      this.tablaMiembros.renderRows()
+    }
+  }
+
+  crearLiga(){
+    if(!this.nombreLiga){
+      alert('Agrega el nombre de tu liga')
+    }else{
+      this.codigo = this.crearCodigo()
+      alert('Tu liga ha sido creada')
+    }
+  }
+
+
+
+  crearCodigo() {
+    var codigo = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 10; i++)
+      codigo += possible.charAt(Math.floor(Math.random() * possible.length)); 
+      return codigo;
+  }
+
+
+  existeMiembro(list, obj) {
+    for (var i in list) {
+      if (JSON.stringify(list[i]) === JSON.stringify(obj)) {
+        return true
+      }
+    }
+    return false
+  }
 }
