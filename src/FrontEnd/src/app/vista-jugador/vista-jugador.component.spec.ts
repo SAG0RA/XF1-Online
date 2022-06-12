a0af6710f0d086fda542b74ef8c5e37fe8e21551
IIISAGORAIII <saulgora9@gmail.com>
2022-06-12 12:01:23 -0600

    Pruebas unitarias

---
+++ src/FrontEnd/src/app/vista-jugador/vista-jugador.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VistaJugadorComponent } from './vista-jugador.component';
+
+describe('VistaJugadorComponent', () => {
+  let component: VistaJugadorComponent;
+  let fixture: ComponentFixture<VistaJugadorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VistaJugadorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VistaJugadorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(VistaJugadorComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
